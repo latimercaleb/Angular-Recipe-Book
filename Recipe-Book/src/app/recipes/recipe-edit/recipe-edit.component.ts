d73cd8628c79884a62fef39d5172fc0097665f7b
latimeks <clatimer@revspringinc.com>
2019-11-05 15:59:19 -0500

    Adding form for reactive approach

--- Recipe-Book/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ Recipe-Book/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,14 +11,36 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editModeEnabled: boolean = false;
-  constructor(private activatedRoute: ActivatedRoute) { }
+  recipeForm: FormGroup;
+  constructor(private activatedRoute: ActivatedRoute, private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.activatedRoute.params.subscribe(
       (params: Params) => {
         this.id = +params.id;
         this.editModeEnabled = params.id != null;
+        this.initForm();
       });
+  }
+
+  private initForm(){
+    let recipeName = '';
+    let recipeImgPath = '';
+    let description = '';
+
+    if (this.editModeEnabled){
+      const recipeToEdit = this.recipeService.getRecipe(this.id);
+      recipeName = recipeToEdit.name;
+      recipeImgPath = recipeToEdit.imagePath;
+      description = recipeToEdit.description;
+    }
+
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imagePath': new FormControl(recipeImgPath),
+      'description': new FormControl(description),
+    });
+
   }
 
 }
