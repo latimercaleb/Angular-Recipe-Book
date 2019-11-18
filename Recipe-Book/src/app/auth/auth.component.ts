e7965c3e230e80eaac70cfbfa8ef7618e6bd97d5
latimercaleb <nohitokaze@gmail.com>
2019-11-18 12:24:22 -0500

    Fixing bug, login behavior

--- Recipe-Book/src/app/auth/auth.component.ts
+++ Recipe-Book/src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { AuthService, AuthResponse } from './auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
@@ -11,13 +12,15 @@ export class AuthComponent implements OnInit {
   loginSelected: boolean;
   loading: boolean;
   error: string;
+  authObserver: Observable<AuthResponse>;
+
   constructor(private authService: AuthService) {
     this.loginSelected = true; // Login page is set by default
     this.loading = false;
     this.error = null;
    }
 
-   swapMode(){
+  swapMode(){
      this.loginSelected = !this.loginSelected;
    }
 
@@ -30,32 +33,26 @@ export class AuthComponent implements OnInit {
       let email = loginData.value.email;
       let pass = loginData.value.password;
       this.loading = true;
-      this.authService.register(email,pass).subscribe(
-        response => {
-          console.log(response);
-          this.loading = false;
-        },
-        errMessage => {
-          this.loading = false;
-          this.error = errMessage;
-        }
-      );
+      this.authObserver = this.authService.register(email,pass);
     }else if (loginData.valid && this.loginSelected){
       let email = loginData.value.email;
       let pass = loginData.value.password;
       this.loading = true;
-      this.authService.login(email,pass).subscribe(
-        response => {
-          console.log('Success');
-          console.log(response);
-          this.loading = false;
-        },
-        errMessage => {
-          this.loading = false;
-          this.error = errMessage;
-        }
-      );
+      this.authObserver = this.authService.login(email,pass);
     }
+    
+    this.authObserver.subscribe(
+      response => {
+        console.log('Success!');
+        console.log(response);
+        this.loading = false;
+        this.error = null;
+      },
+      errMessage => {
+        this.loading = false;
+        this.error = errMessage;
+      }
+    );
     loginData.reset();
   }
 
--- Recipe-Book/src/app/auth/auth.service.ts
+++ Recipe-Book/src/app/auth/auth.service.ts
@@ -1,18 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
-interface AuthResponse {
+export interface AuthResponse {
   idToken: string,
   email: string,
   refreshToken: string,
   expiresIn: string,
-  localId: string
-}
-
-interface loginResponse extends AuthResponse {
-  registered: boolean
+  localId: string,
+  registered?: boolean
 }
 
 @Injectable({
@@ -25,49 +22,41 @@ export class AuthService {
   register(email: string, pass: string){
     return this.http.post<AuthResponse>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDRou91UtXy0Lb_PPU8BOFvjId4d_lKyMs', 
     {email: email, password: pass, returnSecureToken: true})
-    .pipe(
-      catchError(
-        errorObject => {
-          console.log('Error has occured');
-          let returnErrorMessage = 'An undefined error has occured!';
-          if (!errorObject.error || !errorObject.error.error){
-            return throwError(returnErrorMessage);
-          }else{
-            switch(errorObject.error.error.message){
-              case 'EMAIL_EXISTS':
-                returnErrorMessage = `Error: ${errorObject.error.error.code} due to an email existing already`;
-            }
-            return throwError(returnErrorMessage); 
-          }
-        }
-    ));
+    .pipe(catchError(this.customErrorHandler));
   }
 
   login(email: string, pass: string){
-    return this.http.post<loginResponse>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDRou91UtXy0Lb_PPU8BOFvjId4d_lKyMs',
-    {email: email, password: pass, returnSecureToken: true}).pipe(
-      catchError(
-        errorObject => {
-          console.log('Error has occured');
-          let returnErrorMessage = 'An undefined error has occured!';
-          if (!errorObject.error || !errorObject.error.error){
-            return throwError(returnErrorMessage);
-          }else{
-            switch(errorObject.error.error.message){
-              case 'EMAIL_NOT_FOUND':
-                returnErrorMessage = `Error: ${errorObject.error.error.code} due to no email being present`;
-              case 'INVALID_PASSWORD':
-                returnErrorMessage = `Error: ${errorObject.error.error.code} due to an invalid password`;
-              case 'USER_DISABLED':
-                returnErrorMessage = `Error: ${errorObject.error.error.code} due to being disabled`;
-            }
-            return throwError(returnErrorMessage); 
-          }
-        }
-    ));
+    return this.http.post<AuthResponse>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDRou91UtXy0Lb_PPU8BOFvjId4d_lKyMs',
+    {email: email, password: pass, returnSecureToken: true})
+    .pipe(catchError(this.customErrorHandler));
   }
 
   logout(){
 
+  }
+
+  private customErrorHandler(errorObject: HttpErrorResponse){
+    console.log('Error has occured');
+    console.log(errorObject);
+    let returnErrorMessage = 'An undefined error has occured!';
+    if (!errorObject.error || !errorObject.error.error){
+      return throwError(returnErrorMessage);
+    }else{
+      switch(errorObject.error.error.message){
+        case 'EMAIL_NOT_FOUND':
+          returnErrorMessage = `Error: ${errorObject.error.error.code} due to no email being present`;
+          break;
+        case 'INVALID_PASSWORD':
+          returnErrorMessage = `Error: ${errorObject.error.error.code} due to an invalid password`;
+          break;
+        case 'USER_DISABLED':
+          returnErrorMessage = `Error: ${errorObject.error.error.code} due to being disabled`;
+          break;
+        case 'EMAIL_EXISTS':
+          returnErrorMessage = `Error: ${errorObject.error.error.code} due to an email existing already`;
+          break;
+      }
+      return throwError(returnErrorMessage); 
+    }
   }
 }
