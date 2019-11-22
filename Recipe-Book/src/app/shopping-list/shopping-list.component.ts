cf590124f838384e6e4a373b88e0a38dbdd3a9a7
latimercaleb <nohitokaze@gmail.com>
2019-11-22 04:15:29 -0500

    Adding code for service tester

--- Recipe-Book/src/app/shopping-list/shopping-list.component.ts
+++ Recipe-Book/src/app/shopping-list/shopping-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import {Indgredient} from '../shared/indgredient.model';
 import { ShoppingListService } from './shopping-list.service';
 import { Subscription } from 'rxjs';
+import { LoggingService } from '../logging.service';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
@@ -10,13 +11,14 @@ import { Subscription } from 'rxjs';
 export class ShoppingListComponent implements OnInit, OnDestroy {
   indgredients: Indgredient[] = [];
   private indgredientsChangeSubscription: Subscription;
-  constructor(private shoppingListService: ShoppingListService) { }
+  constructor(private shoppingListService: ShoppingListService, private logger: LoggingService) { }
 
   ngOnInit() {
     this.indgredients = this.shoppingListService.getIndgredients();
     this.indgredientsChangeSubscription = this.shoppingListService.indgredientsChanged.subscribe(
       (indgredients: Indgredient[]) => {this.indgredients = indgredients;}
     );
+    this.logger.printLog('Shopping List init');
   }
 
   ngOnDestroy(){
